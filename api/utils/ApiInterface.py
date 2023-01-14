931f06010d9aae3320dfc11b24ddb57b2991b536
Zhu Zijun <2961163526@qq.com>
2023-01-15 02:47:48 +0800

    添加注释 修改自动部署流程

--- api/utils/ApiInterface.py
+++ api/utils/ApiInterface.py
@@ -22,12 +22,18 @@ T = TypeVar("T")
 
 
 class BaseApiResponse(GenericModel, Generic[T]):
+    """
+    API响应模版，回传的ApiResponse子类会填入data中
+    """
     status: int
     msg: str
     data: T | None
 
 
 class ApiResponse(BaseModel):
+    """
+    API响应公共父类，所有响应模型请继承该类
+    """
     pass
 
 
@@ -39,6 +45,18 @@ def api_request(
         query_argument: str = "query",
         **kwargs
 ) -> Callable[[T], T]:
+    """
+    实现请求参数校验即OpenAPI文档生成的装饰器
+
+    **json和form不能同时被设置**
+
+    :param json: 请求体以json格式读取后应当对应的数据模型
+    :param form: 请求体以form格式读取后应当对应的数据模型
+    :param query: 路由查询参数读取后应对应的数据模型
+    :param body_argument: 请求体中参数应当注入的名字，默认为"body"
+    :param query_argument: 路由查询参数应当注入的名字，默认为"body"
+    :param kwargs: 其他需要显示在/docs中的参数（需满足OpenAPI规范）
+    """
     schemas: Dict[str, BaseModel] = {
         key: param
         for key, param in (
@@ -72,7 +90,7 @@ def api_request(
             if inspect.isawaitable(retval):
                 retval = await retval
             return retval
-
+        # 使用sanic-ext中的OperationStore实现文档自动生成，参考其中的openapi.body装饰器
         if body_content is not None:
             if f in OperationStore():
                 OperationStore()[decorated_function] = OperationStore().pop(f)
@@ -84,6 +102,13 @@ def api_request(
 
 def api_response(retval: Optional[Union[Type[ApiResponse], Dict]] = None, status: int = 200, description: str = '',
                  **kwargs):
+    """
+    实现参数返回值自动包装与API页面生成的装饰器
+    :param retval: 返回值信息，可以为ApiResponse的子类或字典，为空则返回值中data项置None
+    :param status: Response Http相应码
+    :param description: 返回值相关描述，显示在/docs页面中
+    :param kwargs: 其他需要显示在/docs中的参数（需满足OpenAPI规范）
+    """
     def decorator(f):
         @wraps(f)
         async def decorated_function(*args, **kwargs):
@@ -94,6 +119,7 @@ def api_response(retval: Optional[Union[Type[ApiResponse], Dict]] = None, status
             kwargs["status"] = status
             return sanic_json(BaseApiResponse(status=1, msg='success', data=ret if ret is not None else {}).dict())
 
+        # 使用sanic-ext中的OperationStore实现文档自动生成，参考其中的openapi.body装饰器
         if f in OperationStore():
             OperationStore()[decorated_function] = OperationStore().pop(f)
 
@@ -109,10 +135,16 @@ def api_response(retval: Optional[Union[Type[ApiResponse], Dict]] = None, status
 
 
 class ApiInterface(HTTPMethodView):
+    """
+    所有API调用的公共父类，API调用请继承该类
+    """
     pass
 
 
 def handle_grpc_error(func):
+    """
+    处理AioRpcError，返回的HttpResponse中包含503与grpc报错详细信息
+    """
     @wraps(func)
     async def wrapped_function(*args, **kwargs):
         try:
